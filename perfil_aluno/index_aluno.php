b44b8269e04a6d59c480de930d05ec4356123e4d
Desenvolvedor 1 <Desenvolvedor 1@DESKTOP-D6D6SM4>
2017-10-10 12:52:24 -0300

    ## 3 - teste 003'

--- perfil_aluno/index_aluno.php
+++ perfil_aluno/index_aluno.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo 'teste de envio dp jayce para mim';
+        echo 'vamu fazer grana';
         
         ?>
     </body>
