f238b1f4c388b5f6ef100adf18487a059b4d6f46
Silvarney <Silvarney@PC-Homer>
2017-10-16 12:10:11 -0300

    #7 - implementação da classe Aluno

--- classes/Aluno.php
+++ classes/Aluno.php
@@ -1,81 +1,263 @@
 <?php
+
 require_once '../conexao/Crud.php';
- 
+
 class Aluno extends Crud {
+
+    private $id_aluno;
     private $nome;
-    private $sexo;
     private $nascimento;
-    private $idade;
-    private $especial;
+    private $sexo;
     private $nis;
-    private $certidao;
+    private $raca;
     private $mae;
     private $pai;
+    private $nacionalidade;
+    private $pais_nasc;
+    private $estado_nasc;
     private $fone;
+    private $municipio_nasc;
+    private $deficiencia;
+    private $tipo_deficiencia;
+    private $identidade;
+    private $complemento_identidade;
+    private $orgao_identidade;
+    private $estado_identidade;
+    private $data_identidade;
+    private $certidao_civil;
+    private $cpf;
+    private $passaporte;
+    private $justificativa;
+    private $localizacao;
+    private $cep;
     private $endereco;
-    private $serie;
-    private $repetente;
+    private $numero;
+    private $complemento;
+    private $bairro;
+    private $estado;
+    private $municipio;
+    private $escolarizacao;
     private $transporte;
+    private $responsavel_transporte;
+    private $tipo_transporte;
     private $status;
-    
+    private $id_escola;
+
+    function setId_Aluno($id_aluno) {
+        $this->id_aluno = $id_aluno;
+    }
+
     function setNome($nome) {
         $this->nome = $nome;
     }
-    
-    function setSexo($sexo) {
-        $this->sexo = $sexo;
-    }
-    
+
     function setNascimento($nascimento) {
         $this->nascimento = $nascimento;
     }
-    
-    function setIdade($idade) {
-        $this->idade = $idade;
+
+    function setSexo($sexo) {
+        $this->sexo = $sexo;
     }
-    
-    function setEspecial($especial) {
-        $this->especial = $especial;
-    }
-    
+
     function setNis($nis) {
         $this->nis = $nis;
     }
-    
-    function setCertidao($certidao) {
-        $this->certidao = $certidao;
+
+    function setRaca($raca) {
+        $this->raca = $raca;
     }
-    
+
     function setMae($mae) {
         $this->mae = $mae;
     }
-    
+
     function setPai($pai) {
         $this->pai = $pai;
     }
-    
+
+    function setNacionalidade($nacionalidade) {
+        $this->nacionalidade = $nacionalidade;
+    }
+
+    function setPais_nasc($pais_nasc) {
+        $this->pais_nasc = $pais_nasc;
+    }
+
+    function setEstado_nasc($pais_nasc) {
+        $this->pais_nasc = $pais_nasc;
+    }
+
     function setFone($fone) {
         $this->fone = $fone;
     }
-    
+
+    function setMunicipio_nasc($municipio_nasc) {
+        $this->municipio_nasc = $municipio_nasc;
+    }
+
+    function setDeficiencia($deficiencia) {
+        $this->deficiencia = $deficiencia;
+    }
+
+    function setTipo_deficiencia($tipo_deficiencia) {
+        $this->tipo_deficiencia = $tipo_deficiencia;
+    }
+
+    function setIdentidade($identidade) {
+        $this->identidade = $identidade;
+    }
+
+    function setComplemento_identidade($complemento_identidade) {
+        $this->complemento_identidade = $complemento_identidade;
+    }
+
+    function setOrgao_identidade($orgao_identidade) {
+        $this->orgao_identidade = $orgao_identidade;
+    }
+
+    function setEstado_identidade($estado_identidade) {
+        $this->estado_identidade = $estado_identidade;
+    }
+
+    function setData_identidade($data_identidade) {
+        $this->data_identidade = $data_identidade;
+    }
+
+    function setCertidao_civil($certidao_civil) {
+        $this->certidao_civil = $certidao_civil;
+    }
+
+    function setCPF($cpf) {
+        $this->cpf = $cpf;
+    }
+
+    function setPassaporte($passaporte) {
+        $this->passaporte = $passaporte;
+    }
+
+    function setJustificativa($justificativa) {
+        $this->justificativa = $justificativa;
+    }
+
+    function setLocalizacao($localizacao) {
+        $this->localizacao = $localizacao;
+    }
+
+    function setCEP($cep) {
+        $this->cep = $cep;
+    }
+
     function setEndereco($endereco) {
         $this->endereco = $endereco;
     }
-    
-    function setSerie($serie) {
-        $this->serie = $serie;
+
+    function setNumero($numero) {
+        $this->numero = $numero;
     }
-    
-    function setRepetente($repetente) {
-        $this->repetente = $repetente;
+
+    function setComplemento($complemento) {
+        $this->complemento = $complemento;
     }
-    
+
+    function setBairro($bairro) {
+        $this->bairro = $bairro;
+    }
+
+    function setEstado($estado) {
+        $this->estado = $estado;
+    }
+
+    function setMunicipio($municipio) {
+        $this->municipio = $municipio;
+    }
+
+    function setEscolarizacao($escolarizacao) {
+        $this->escolarizacao = $escolarizacao;
+    }
+
     function setTransporte($transporte) {
         $this->transporte = $transporte;
     }
-    
+
+    function setResponsavel_transporte($responsavel_transporte) {
+        $this->responsavel_transporte = $responsavel_transporte;
+    }
+
+    function setTipo_transporte($tipo_transporte) {
+        $this->tipo_transporte = $tipo_transporte;
+    }
+
     function setStatus($status) {
         $this->status = $status;
     }
-    
+
+    function setId_escola($id_escola) {
+        $this->id_escola = $id_escola;
+    }
+
+    public function inserir_aluno() {
+        try {
+            $sql = "INSERT INTO aluno(id_aluno, nome_aluno, nascimento_aluno, sexo_aluno, nis_aluno,"
+                    . " raca_aluno, mae_aluno, pai_aluno, nacionalidade_aluno, pais_nasc_aluno,"
+                    . " estado_nasc_aluno, fone_aluno, municipio_nasc_aluno, deficiencia_aluno,"
+                    . " tipo_deficiencia_aluno, identidade_aluno, complemento_identidade_aluno,"
+                    . " orgao_identidade_aluno, estado_identidade_aluno, data_identidade_aluno,"
+                    . " certidao_civil_aluno, cpf_aluno, passaporte_aluno, justificativa_aluno,"
+                    . " localizacao_aluno, cep_aluno, endereco_aluno, numero_aluno, complemento_aluno,"
+                    . " bairro_aluno, estado_aluno, municipio_aluno, outra_escolarizacao_aluno, transporte_aluno,"
+                    . " responsavel_transporte_aluno, tipo_transporte_aluno, status_aluno, escola_id_escola) "
+                    
+                    . "VALUES (:id_aluno, :nome, :nascimento, :sexo, :nis, :raca, :mae, :pai, :nacionalidade, :pais_nasc,"
+                    . " :estado_nasc, :fone, :municipio_nasc, :deficiencia, :tipo_deficiencia, :identidade, :complemento_identidade,"
+                    . " :orgao_identidade, :estado_identidade, :data_identidade, :certidao_civil, :cpf, :passaporte,"
+                    . " :justificativa, :localizacao, :cep, :endereco, :numero, :complemento, :bairro, :estado, :municipio,"
+                    . " :escolarizacao, :transporte, :responsavel_transporte, :tipo_transporte, :status, :id_escola)";
+            
+            $stmt = DB::prepare($sql);
+            $stmt->bindParam(':id_aluno', $this->id_aluno);
+            $stmt->bindParam(':nome', $this->nome);
+            $stmt->bindParam(':nascimento', $this->nascimento);
+            $stmt->bindParam(':sexo', $this->sexo);
+            $stmt->bindParam(':nis', $this->nis);
+            $stmt->bindParam(':raca', $this->raca);
+            $stmt->bindParam(':mae', $this->mae);
+            $stmt->bindParam(':pai', $this->pai);
+            $stmt->bindParam(':nacionalidade', $this->nacionalidade);
+            $stmt->bindParam(':pais_nasc', $this->pais_nasc);
+            $stmt->bindParam(':estado_nasc', $this->estado_nasc);
+            $stmt->bindParam(':fone', $this->fone);
+            $stmt->bindParam(':municipio_nasc', $this->municipio_nasc);
+            $stmt->bindParam(':deficiencia', $this->deficiencia);
+            $stmt->bindParam(':tipo_deficiencia', $this->tipo_deficiencia);
+            $stmt->bindParam(':identidade', $this->identidade);
+            $stmt->bindParam(':complemento_identidade', $this->complemento_identidade);
+            $stmt->bindParam(':orgao_identidade', $this->orgao_identidade);
+            $stmt->bindParam(':estado_identidade', $this->estado_identidade);
+            $stmt->bindParam(':data_identidade', $this->data_identidade);
+            $stmt->bindParam(':certidao_civil', $this->certidao_civil);
+            $stmt->bindParam(':cpf', $this->cpf);
+            $stmt->bindParam(':passaporte', $this->passaporte);
+            $stmt->bindParam(':justificativa', $this->justificativa);
+            $stmt->bindParam(':localizacao', $this->localizacao);
+            $stmt->bindParam(':localizacao', $this->cep);
+            $stmt->bindParam(':endereco', $this->endereco);
+            $stmt->bindParam(':numero', $this->numero);
+            $stmt->bindParam(':complemento', $this->complemento);
+            $stmt->bindParam(':bairro', $this->bairro);
+            $stmt->bindParam(':estado', $this->estado);
+            $stmt->bindParam(':municipio', $this->municipio);
+            $stmt->bindParam(':escolarizacao', $this->escolarizacao);
+            $stmt->bindParam(':transporte', $this->transporte);
+            $stmt->bindParam(':responsavel_transporte', $this->responsavel_transporte);
+            $stmt->bindParam(':responsavel_transporte', $this->responsavel_transporte);
+            $stmt->bindParam(':tipo_transporte', $this->tipo_transporte);
+            $stmt->bindParam(':status', $this->status);
+            $stmt->bindParam(':id_escola', $this->id_escola);
+            return $stmt->execute();
+        } catch (Exception $ex) {
+            echo 'Falha ao inserir Aluno<br>';
+            echo $ex->getMessage();
+        }
+    }
+
 }
